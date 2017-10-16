6d189b3f25d7a09f2130fa8d7cce86194600705d
Martin Halamíček <martin@halamicek.cz>
2017-10-16 12:08:35 +0200

    fix - deploy app id

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,18 @@ docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
-  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ag-dark-sky`
+  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.dark-sky-augmentation`
 docker tag keboola/darksky-augmentation:latest $REPOSITORY:$TRAVIS_TAG
 docker tag keboola/darksky-augmentation:latest $REPOSITORY:latest
 eval $(docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
-  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ag-dark-sky)
+  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.dark-sky-augmentation)
 docker push $REPOSITORY:$TRAVIS_TAG
 docker push $REPOSITORY:latest
 
 docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
-  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola keboola.ag-dark-sky $TRAVIS_TAG
+  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola keboola.dark-sky-augmentation $TRAVIS_TAG
 
